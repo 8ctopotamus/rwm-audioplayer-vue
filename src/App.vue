5caf63e7fbfad98591abf50303021d160edb1d70
8ctopotamus <joshnaylor88@gmail.com>
2018-06-13 16:32:17 -0500

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,182 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <radar-spinner
+      id="loading"
+      v-if="loading"
+      :animation-duration="2000"
+      :size="60"
+      :color="color"
+    />
+    <aplayer
+      v-else
+      autoplay
+      :music="currentPodcast"
+      :list="podcasts"
+      :shuffle="false"
+    >
+      <span slot="display" >
+        Episode description here...
+      </span>
+    </aplayer>
   </div>
 </template>
 
 <script>
+import Aplayer from 'vue-aplayer'
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import moment from 'moment'
+import { RadarSpinner } from 'epic-spinners'
+
+const API_URL = 'https://realwealthmarketing.com/wp-json/wp/v2'
+const CURRENT_DATE = moment().format('YYYY-MM-DD')
+const ONE_YEAR_AGO = moment().subtract(1, 'year').format('YYYY-MM-DD')
+const TWO_YEARS_AGO = moment().subtract(2, 'years').format('YYYY-MM-DD')
 
 export default {
   name: 'app',
+  props: [
+    'autoplay',
+    'color',
+    'playlist',
+    'slug'
+  ],
+  data () {
+    return {
+      loading: true,
+      error: false,
+      advisor: null,
+      group: null,
+      frequency: null,
+      podcastDBPageNum: 1,
+      podcasts: [],
+      currentPodcast: {},
+    }
+  },
+  mounted () {
+    this.getAdvisorData()
+  },
+  methods: {
+    getAdvisorData () {
+      axios.get(`${API_URL}/users?slug=${this.slug}`)
+        .then(res => {
+          if (res.data.length === 0)
+            return this.error = true
+
+          this.advisor = res.data[0]
+
+          switch(this.advisor.acf.compliance_group) {
+            case 'generic':
+              this.group = '?group-access=68'
+              break
+            case 'bifs':
+              this.group = '?group-access=125'
+              break
+            case 'brookstone':
+              this.group = '?group-access=113'
+              break
+            case 'cambridge':
+              this.group = '?group-access=104'
+              break
+            case 'cfd':
+              this.group = '?group-access=112'
+              break
+            case 'firstheartlandcapital':
+              this.group = '?group-access=60'
+              break
+            case 'harbour':
+    					group = '?group-access=136'
+    					break
+            case 'garepple':
+              this.group = '?group-access=109'
+              break
+            case 'gretchenevans':
+              this.group = '?group-access=61'
+              break
+            case 'theleadersgroup':
+              this.group = '?group-access=111'
+              break
+            case 'legacy':
+              this.group = '?group-access=133'
+              break
+            case 'mholdings':
+    					group = '?group-access=135'
+    					break
+            case 'naifa':
+              this.group = '?group-access=62'
+              break
+            case 'nationalplanningcorporation':
+              this.group = '?group-access=63'
+              break
+            case 'nextfinancial':
+              this.group = '?group-access=57'
+              break
+            case 'oneamericaib':
+              this.group = '?group-access=67'
+              break
+            case 'onesco':
+              this.group = '?group-access=101'
+              break
+            case 'prosperitycapitaladvisors':
+              this.group = '?group-access=59'
+              break
+            case 'questarcapitalassets':
+              this.group = '?group-access=64'
+              break
+            case 'signator':
+              this.group = '?group-access=132'
+              break
+            case 'sfa':
+              this.group = '?group-access=110'
+              break
+            case 'woodburyfinancialservices':
+              this.group = '?group-access=66'
+              break
+            case 'voya':
+              this.group = '?group-access=131'
+              break
+            default:
+              group = ''
+          }
+
+          if (this.advisor.acf.podcast_frequency === 'weekly') {
+            this.frequency = '&podcast-frequency=51'
+          }
+          else {
+            this.frequency = '&podcast-frequency=52'
+          }
+
+          this.getPodcasts()
+        })
+        .catch(err => {
+          console.log(err)
+          return this.error = err
+        })
+    },
+    getPodcasts () {
+      axios.get(`${API_URL}/podcasts${this.group}${this.frequency}&page=${this.podcastDBPageNum}`)
+        .then(res => {
+          this.podcasts = res.data.map(pod => {
+            return {
+              title: pod.title.rendered,
+              artist: pod.acf.guest_info[0].guest_name,
+              src: pod.acf.podcast_file,
+              pic: pod.better_featured_image.media_details.sizes.medium.source_url
+            }
+          })
+          this.currentPodcast = this.podcasts[0]
+          this.loading = false
+        })
+        .catch(err => {
+          console.log(err)
+          return this.error = err
+        })
+    }
+  },
   components: {
+    Aplayer,
+    RadarSpinner,
     HelloWorld
-  }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './scss/main.scss'
+
 Vue.config.productionTip = false
 
+// get player options
+const slug = document.getElementById('rw-player').dataset.slug || 'sg'
+const color = document.getElementById('rw-player').dataset.color || 'blue'
+const playlist = document.getElementById('rw-player').dataset.playlist || true
+const autoplay = document.getElementById('rw-player').dataset.autoplay || false
+
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: '#rw-player',
+  data () {
+    return {
+      autoplay,
+      color,
+      playlist,
+      slug
+    }
+  },
+  render: function (createElement) {
+    return createElement(App, {
+      props: {
+        autoplay: this.autoplay,
+        color: this.color,
+        playlist: this.playlist,
+        slug: this.slug
+      }
+    })
+  }
+})
