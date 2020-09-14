4b2387f086a3640c3c577d364ffe998eee72e106
Josh <zylo.codes@gmail.com>
2020-09-14 07:25:56 -0500

    Puts CTA button first

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="action-buttons">
-    <button @click="$emit('show-modal', 'moreInfo')" :style="{background: color}">More Info</button>
-    <button @click="$emit('show-modal', 'subscribe')" :style="{background: color}">Subscribe</button>
-    <button @click="$emit('show-modal', 'forwardToAFriend')" :style="{background: color}">Forward to a friend</button>
-    <a :href="podcast.src" download>
-      <button :style="{background: color}">Download Episode</button>
-    </a>
     <a 
       v-show="advisor.acf.cta_link"
       :href="advisor.acf.cta_link"
@@ -15,6 +9,12 @@
       <button :style="{background: color}">
         {{advisor.acf.cta_text ? advisor.acf.cta_text : 'Learn More'}}
       </button>
+    </a>
+    <button @click="$emit('show-modal', 'moreInfo')" :style="{background: color}">More Info</button>
+    <button @click="$emit('show-modal', 'subscribe')" :style="{background: color}">Subscribe</button>
+    <button @click="$emit('show-modal', 'forwardToAFriend')" :style="{background: color}">Forward to a friend</button>
+    <a :href="podcast.src" download>
+      <button :style="{background: color}">Download Episode</button>
     </a>
   </div>
 </template>
